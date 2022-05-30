6743a6534bcba38daeb946d0d959689cd0b5be3d
FthRobby <rbysalam10@gmail.com>
2022-05-31 02:59:35 +0700

    push file

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  KEY: "fcb1c43e997be90a8aa17e12e0aaa67c",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+  DATABASE_NAME: "movie-catalogue-database",
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: "movies",
+  WEB_SOCKET_SERVER: "wss://movies-feed.dicoding.dev",
+};
+
+export default CONFIG;
